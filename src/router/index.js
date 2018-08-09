428ee2f73ce3ff0215858190a132944dccb2335d
songke <1154285845@qq.com>
2018-08-09 23:43:55 +0800

    home路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login';
+import Home from '@/views/Home';
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    { name: 'Home', path: '/', component: Home },
     { name: 'Login', path: '/login', component: Login }
   ]
 });
