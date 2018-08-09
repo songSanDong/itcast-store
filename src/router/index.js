c6820f3b9353fdd7f1fa19c33f9186a416fd7b2f
songke <1154285845@qq.com>
2018-08-10 01:35:55 +0800

    配置 Home 路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,20 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login';
 import Home from '@/views/Home';
+import Users from '@/views/Users/Users';
 //配置路由规则
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { name: 'Home', path: '/', component: Home },
-    { name: 'Login', path: '/login', component: Login }
+    { name: 'Login', path: '/login', component: Login },
+    {
+      name: 'Home',
+      path: '/',
+      component: Home,
+      children: [
+        { name: 'Users', path: 'users', component: Users }
+      ]
+    }
   ]
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -101,7 +101,9 @@
           </el-menu>
         </el-col>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
