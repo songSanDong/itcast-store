1d074a054ceee40e909f14c96ac4ad91d245c934
songke <1154285845@qq.com>
2018-08-10 20:39:24 +0800

    Vue的插件机制

--- src/main.js
+++ src/main.js
@@ -2,22 +2,31 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+// 引入路由
 import router from './router';
+// 引入布局组件
 import ElementUI from 'element-ui';
+// 全局样式
 import 'element-ui/lib/theme-chalk/index.css';
+// 全局样式
 import './assets/css/style.css';
+// 处理时间戳
 import moment from 'moment';
+// 引入 axios 发送异步请求
 import axios from 'axios';
+// 注册 vue 插件
+import myaxios  from './plugins/MyAxios.js';
 
 Vue.config.productionTip = false;
+// 注册插件
 Vue.use(ElementUI);
 /* eslint-disable no-new */
+// 注册地址, 全局 axios 插件
+Vue.use(myaxios);
+// 时间戳处理
 Vue.filter('fmtDate', (value, fmtString) => {
   return moment(value).format(fmtString);
 });
-// 配置全局的axios
-axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/';
-Vue.prototype.$http = axios;
 new Vue({
   el: '#app',
   router,
