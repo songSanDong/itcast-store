ed000e47d9dc080fd74fad3f4e932093b5da0263
songke <1154285845@qq.com>
2018-08-14 15:02:56 +0800

    设置请求拦截器

--- src/plugins/MyAxios.js
+++ src/plugins/MyAxios.js
@@ -5,5 +5,27 @@ MyAxios.install = function (Vue) {
   // 配置全局的axios
   axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/';
   Vue.prototype.$http = axios;
+  // Add a request interceptor
+  // 在axios官网有说明, interceptors 请求拦截器
+  axios.interceptors.request.use(function (config) {
+      // Do something before request is sent
+      if(config.url.toLocaleLowerCase !== 'login') {
+        const token = sessionStorage.getItem('token');
+        config.headers.Authorization = token;
+      }
+      return config;
+    }, function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    });
+
+  // Add a response interceptor
+  axios.interceptors.response.use(function (response) {
+      // Do something with response data
+      return response;
+    }, function (error) {
+      // Do something with response error
+      return Promise.reject(error);
+    });
 };
 export default MyAxios;
