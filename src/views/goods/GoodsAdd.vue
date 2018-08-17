2824bd1432297456f977cc8ee1ff61e9295358c2
songke <1154285845@qq.com>
2018-08-17 22:28:11 +0800

    步骤条和tab栏

--- src/views/goods/GoodsAdd.vue
+++ src/views/goods/GoodsAdd.vue
@@ -1,13 +1,54 @@
 <template>
   <el-card class="card">
-    GoodsAdd
+    <!-- 面包屑 -->
+    <my-breadcrumb level1="权限管理" level2="权限列表"></my-breadcrumb>
+    <el-alert
+      title="添加商品信息"
+      type="info"
+      center
+      show-icon>
+    </el-alert>
+    <el-steps
+      :active="active = 0"
+      style="width:1000px;margin:20px 0"
+      align-center
+      finish-status="success">
+      <el-step title="步骤 1"></el-step>
+      <el-step title="步骤 2"></el-step>
+      <el-step title="步骤 3"></el-step>
+      <el-step title="步骤 4"></el-step>
+      <el-step title="步骤 5"></el-step>
+    </el-steps>
+    <el-tabs
+      @click="handleTabClick"
+      tab-position="left"
+      style="height: 200px;">
+      <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+      <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+      <el-tab-pane label="商品属性">商品属性</el-tab-pane>
+      <el-tab-pane label="商品图片">商品图片</el-tab-pane>
+      <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+    }
+  }
 };
 </script>
 <style>
-
+  .card {
+    height: 100%;
+  }
+  .el-breadcrumb {
+    background-color: #D3DCE6;
+    height: 45px;
+    line-height: 45px;
+  }
+  .el-card__body {
+    padding: 0;
+  }
 </style>
